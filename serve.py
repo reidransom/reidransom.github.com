98f49ff4c18cca7df2eff8dfbd9d8cd40ceadc1f
reidransom <reid@reidransom.com>
2014-10-16 22:40:00 -0700

    update for python 3

--- serve.py
+++ serve.py
@@ -1,59 +1,49 @@
 #!/usr/bin/env python
- 
+
 import os
 import sys
 import argparse
 import subprocess
-import BaseHTTPServer
-import CGIHTTPServer
-import cgitb; cgitb.enable()  ## This line enables CGI error reporting
 
 # SSH Deploy Destination:
-#destination = 'user@example.com:path/to/docroot/'
 destination = 'reidransom@reidransom.webfactional.com:webapps/reidransom/'
 
 def parseArgs():
-	parser = argparse.ArgumentParser(description="Run a static site development http server.")
-	parser.add_argument('--deploy', action='store_const', default=False, const=True, dest='deploy', help='sync with the production server')
-	parser.add_argument('-s', '--serve', action='store_const', default=False, const=True, dest='serve', help='start the http server')
-	parser.add_argument('-o', '--open', action='store_const', default=False, const=True, dest='open', help='open the root url in the default browser')
-	parser.add_argument('-p', '--port', action='store', default=8000, metavar='PORT', dest='port', help='port number to run the http server on')
-	parser.add_argument('-d', '--docroot', action='store', default="./public", metavar='DOCROOT', dest='docroot', help='directory where files will be served from')
-	return parser.parse_args()
+    parser = argparse.ArgumentParser(description="Run a static site development http server.")
+    parser.add_argument('--deploy', action='store_const', default=False, const=True, dest='deploy', help='sync with the production server')
+    parser.add_argument('-s', '--serve', action='store_const', default=False, const=True, dest='serve', help='start the http server')
+    parser.add_argument('-o', '--open', action='store_const', default=False, const=True, dest='open', help='open the root url in the default browser')
+    parser.add_argument('-p', '--port', action='store', default=8000, metavar='PORT', dest='port', help='port number to run the http server on')
+    parser.add_argument('-d', '--docroot', action='store', default="./public", metavar='DOCROOT', dest='docroot', help='directory where files will be served from')
+    return parser.parse_args()
 
 def getURL(port):
-	return 'http://localhost:%d/' % port
+    return 'http://localhost:%d/' % port
 
 def serve(port, docroot):
-	os.chdir(docroot) 
-	server = BaseHTTPServer.HTTPServer
-	handler = CGIHTTPServer.CGIHTTPRequestHandler
-	server_address = ("", port)
-	handler.cgi_directories = [""]
-	httpd = server(server_address, handler)
-	print 'starting server at %s' % getURL(port)
-	try:
-		httpd.serve_forever()
-	except KeyboardInterrupt:
-		print 'goodbye'
+    os.chdir(docroot)
+    subprocess.call(['http-server', '-p', str(port)])
 
 def openInBrowser(url):
-	subprocess.call(['open', url])
+    try:
+        subprocess.call(['open', url])
+    except FileNotFoundError:
+        pass
 
 def deploy():
-	subprocess.call(['rsync', '-av', '--delete', '--delete-excluded', '--exclude', '*.DS_Store', '-e', 'ssh', './public/', destination])
+    subprocess.call(['rsync', '-av', '--delete', '--delete-excluded', '--exclude', '*.DS_Store', '-e', 'ssh', './public/', destination])
 
 def run():
-	args = parseArgs()
-	if args.deploy == True:
-		deploy()
-	if args.open == True:
-		openInBrowser(getURL(args.port))
-	if args.serve == True:
-		serve(args.port, args.docroot)
-	if len(sys.argv) < 2:
-		openInBrowser(getURL(args.port))
-		serve(args.port, args.docroot)
+    args = parseArgs()
+    if args.deploy == True:
+        deploy()
+    if args.open == True:
+        openInBrowser(getURL(args.port))
+    if args.serve == True:
+        serve(args.port, args.docroot)
+    if len(sys.argv) < 2:
+        openInBrowser(getURL(args.port))
+        serve(args.port, args.docroot)
 
 if __name__ == "__main__":
-	run()
+    run()
